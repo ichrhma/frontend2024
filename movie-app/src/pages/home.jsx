130e6b7553a4e0bfe579709b2f2b64aac626afeb
ichrhma <163158185+ichrhma@users.noreply.github.com>
2024-04-25 18:37:41 +0700

    Add files via upload

---
+++ movie-app/src/pages/home.jsx
@@ -0,0 +1,14 @@
+import AddMovieForm from "../componen/AddMovie";
+import Navbar from "../componen/navbar";
+
+function Home() {
+return(
+    <>
+    <Navbar></Navbar>
+    <AddMovie></AddMovie>
+    <h1>Home</h1>
+    </>
+);
+}
+
+export default Home;
